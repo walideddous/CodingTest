04cd730e5973a842b55b04a4b8d15a72b514f50c
walid <walid.eddous@gmail>
2020-06-17 22:20:13 +0200

    ss

--- sendEmail.js
+++ sendEmail.js
@@ -1,10 +1,10 @@
 // I started by importing the function emailsList
 const emailsList = require('./emailsList');
 
-/*this function will simulate the sending of emails by browsing the list
+/*this function will simulate the sending of emails by going throw the list
 of emails then with the predefined function setIntervale which will allow
 us to control the repetitive execution time of a function which is 500 milliseconds
-and to display on the console a message of confirmation of the sending of the emails.*/
+and to display on the console a message of confirmation of the emails sending .*/
 
 /* explains how the sendEmail function works:
 => declared a counter has 0 
